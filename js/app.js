6703c80c770d04cc504655869621d642af96ebf5
Souzooka <bazookaharwell@gmail.com>
2017-01-19 21:32:10 -1000

    Exercise 5

--- js/app.js
+++ js/app.js
@@ -24,8 +24,7 @@ document.getElementsByClassName("profile")[0].innerHTML = "Lorem ipsum dolizzle 
 
 /*5. Relace the n/a for Samuel L Jackson with his lorem ipsum*/
 
-
-
+document.getElementsByClassName("profile")[2].innerHTML = "Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in American crime. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like they're actually proud of that shit.";
 
 /*6. Create a div element for Chuck Norris and give it an id of "name7". Inside this div element, give it the contents of "Chuck Norris".
 
